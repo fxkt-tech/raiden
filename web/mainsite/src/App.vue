e76670c2101ef3e9ca26051fd9bcb032b13f6ed7
dengxiaochuan <1554694323@qq.com>
2022-04-05 20:25:12 +0800

    feat(web): create mainsite

---
+++ web/mainsite/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view></router-view>
+</template>
+<script setup>
+</script>
+<style>
+#app {
+  font-family: QingNingYouYuan, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+}
+html {
+  background-color: #f4f5f7;
+}
+</style>
